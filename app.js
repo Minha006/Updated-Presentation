fb2376598f15366282d4bf304eb711dff923adef
Nageen Yousuf <homayonyousaf@gmail.com>
2023-05-25 13:46:54 +0500

    Updated app.js

---
+++ app.js
@@ -0,0 +1,74 @@
+//comparison operator 
+//---"Greater than"----
+//------->------
+//Example :1
+let x = 45;
+let y = 50;
+console.log(x > y);
+
+//----"Less than"----
+//-----<----------
+//Example : 1
+let num1 = 6;
+let num2 = 7
+console.log(num1 < num2);
+
+//----"Greater than equals to"-----
+//------->=-------
+//Example : 1 
+  var yourAge = 19;
+    if (yourAge >= 18) {
+    document.write("You are eligible to vote.");
+   } else {
+    document.write("You are not yet eligible to vote.");
+    };
+
+//Example : 2
+    let marks = 85;
+    if (marks >= 90) {
+      console.log("You received an A grade.");
+    } else if (marks >= 80) {
+      console.log("You received a B grade.");
+    } else if (marks >= 70) {
+      console.log("You received a C grade.");
+    } else {
+      console.log("You need to improve your score.");
+    }
+    //-----"Question to be asked"-------
+    let points = 75;
+    let passingPoints = 60;
+     if (points >= passingPoints) {
+      
+  console.log("Congratulations! You passed the exam.");
+} else {
+  console.log("Unfortunately, you did not pass the exam.");
+}  
+
+//----"Less than equals to"-----
+     //------<=-----
+  //Example : 1
+     let age = 18;
+if (age <= 18) {
+  console.log("You are eligible for a younger discount.");
+} else {
+  console.log("Regular ticket prices apply.");
+}
+
+//Example : 2
+   let childAge = 5;
+   if(age<= 5){
+    console.log("Your child can take polio drops")
+   }else{
+    console.log("No need for polio drops");
+   }
+
+  //------"Question to be asked"------
+   let temp = 38;
+   if(temp <= 38){
+    document.write("Lets go to have fun")
+}else{
+   document.write("It's better to stay at home")
+
+}
+
+
